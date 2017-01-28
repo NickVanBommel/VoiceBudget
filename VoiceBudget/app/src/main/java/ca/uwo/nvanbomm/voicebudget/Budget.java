1062608b2c7db3a4ddb8c9522e04c147075dfc23
Andrew <Andy.keech@live.ca>
2017-01-28 15:27:55 -0500

    Fixed budget class

--- VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/Budget.java
+++ VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/Budget.java
@@ -1,8 +1,52 @@
-package ca.uwo.nvanbomm.voicebudget;
+package main.java.ca.uwo.nvanbomm.voicebudget;
 
 /**
  * Created by andyk on 2017-01-28.
  */
 
 public class Budget {
+    int limit;
+    int balance;
+    String name;
+
+    //Constructors-----------------------------------------------------------------------
+
+    public Budget(int cLimit, String cName){
+        limit = cLimit;
+        name=cName;
+    }
+
+
+    //General Functions-------------------------------------------------------------------
+
+    public void spend(int amount){
+        balance += amount;
+    }
+
+    //Get/Set functions-------------------------------------------------------------------
+
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
