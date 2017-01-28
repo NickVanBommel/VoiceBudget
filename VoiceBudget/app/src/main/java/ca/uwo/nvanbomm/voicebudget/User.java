75ad7b64581daed52b90f3f8e744948ae0c07e49
dzagar <dzagar@uwo.ca>
2017-01-28 15:14:32 -0500

    Fixed User file, Andy's git is fckd

--- VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
+++ VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
@@ -11,21 +11,17 @@ import ca.uwo.nvanbomm.voicebudget.Budget;
 
 public class User {
     int balance;
-    //Define budgets
     ArrayList<Budget> userBudgets;
 
 
     User() {
         balance = 10000;
         userBudgets = new ArrayList<Budget>();
-        //userBudgets.add(new Budget(75*100, "transportation"));
-        //userBudgets.add(new Budget(150*100, "fun"));
-        //userBudgets.add(new Budget(100*100, "food"));
-        //userBudgets.add(new Budget(400*100, "groceries"));
+        userBudgets.add(new Budget(75*100, "transportation"));
+        userBudgets.add(new Budget(150*100, "fun"));
+        userBudgets.add(new Budget(100*100, "food"));
+        userBudgets.add(new Budget(400*100, "groceries"));
     }
-
-    //Check BudgetRemainder(str: budgetName) returns float
-    //Check IsFeasiblePurchase(float: cost, str: budgetName) returns bool
 
     int GetGeneralBalance(){
         return balance;
@@ -33,19 +29,17 @@ public class User {
 
     int GetBudgetLimit(String budgetName){
         for(int i = 0; i < userBudgets.size(); i++){
-            //if (userBudgets[i].getName() == budgetName){
-                //return userBudgets[i].getLimit();
-            //}
+            if (userBudgets.get(i).getName() == budgetName){
+                return userBudgets[i].getLimit();
+            }
         }
-        return 0;
     }
     int GetBudgetBalance(String budgetName){
         for(int i = 0; i < userBudgets.size(); i++){
-            //if (userBudgets[i].getName() == budgetName){
-                //return userBudgets[i].getBalance();
-            //}
+            if (userBudgets.get(i).getName() == budgetName){
+                return userBudgets[i].getBalance();
+            }
         }
-        return 0;
     }
 
     int GetBudgetRemainder(String budgetName){
