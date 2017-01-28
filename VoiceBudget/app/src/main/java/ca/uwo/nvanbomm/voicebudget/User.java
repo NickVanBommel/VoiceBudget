043847ca8abf607b8fbf8fb8c2e9f5d293f83fc4
dzagar <dzagar@uwo.ca>
2017-01-28 14:44:57 -0500

    Added default Budget examples to User, wrote balance retrieval and budget balance retrieval functions

--- VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
+++ VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
@@ -4,19 +4,46 @@ package ca.uwo.nvanbomm.voicebudget;
  * Created by nicho on 1/28/2017.
  */
 
+
 public class User {
-    int balance;
+    float balance;
     //Define budgets
-    ArrayList<Budget> userBudgets = new ArrayList<Budget>();
+    ArrayList<Budget> userBudgets;
+
 
     User() {
         balance = 10000;
+        userBudgets = new ArrayList<Budget>();
+        userBudget.add(new Budget("transportation", 75));
+        userBudget.add(new Budget("fun", 150));
+        userBudget.add(new Budget("food", 100));
+        userBudget.add(new Budget("groceries", 400);
     }
 
     //Get GeneralBalance
     //Get BudgetBalance(str: budgetName) returns float
     //Check BudgetRemainder(str: budgetName) returns float
     //Check IsFeasiblePurchase(float: cost, str: budgetName) returns bool
+
+    float GetGeneralBalance(){
+        return balance;
+    }
+
+    float GetBudgetBalance(String budgetName){
+        for(int i = 0; i < userBudgets.size(); i++){
+            if (userBudgets[i].getName() == budgetName){
+                return userBudgets[i].getBalance();
+            }
+        }
+    }
+
+    float GetBudgetRemainder(String budgetName){
+
+    }
+
+    boolean IsFeasiblePurchase(float cost, String budgetName){
+
+    }
 
 
 }
