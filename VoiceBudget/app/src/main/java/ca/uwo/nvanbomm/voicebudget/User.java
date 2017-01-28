aaadb04e7031c7330556f11b521c9d69111a0aa9
dzagar <dzagar@uwo.ca>
2017-01-28 14:22:13 -0500

    Added proper Budget list, writing skeleton User functions

--- VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
+++ VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/User.java
@@ -6,8 +6,17 @@ package ca.uwo.nvanbomm.voicebudget;
 
 public class User {
     int balance;
+    //Define budgets
+    ArrayList<Budget> userBudgets = new ArrayList<Budget>();
 
     User() {
         balance = 10000;
     }
+
+    //Get GeneralBalance
+    //Get BudgetBalance(str: budgetName) returns float
+    //Check BudgetRemainder(str: budgetName) returns float
+    //Check IsFeasiblePurchase(float: cost, str: budgetName) returns bool
+
+
 }
