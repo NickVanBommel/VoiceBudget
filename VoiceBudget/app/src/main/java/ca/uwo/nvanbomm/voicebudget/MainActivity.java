7944f0891172c48f24664e4880690cfc8706a5f1
dzagar <dzagar@uwo.ca>
2017-01-28 19:31:49 -0500

    Deleted ALL THE THINGS

--- VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/MainActivity.java
+++ VoiceBudget/app/src/main/java/ca/uwo/nvanbomm/voicebudget/MainActivity.java
@@ -2,103 +2,28 @@ package ca.uwo.nvanbomm.voicebudget;
 
 import android.net.Uri;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-
-import ai.api.android.AIConfiguration;
-import ai.api.model.AIError;
-import ai.api.model.AIResponse;
-import ai.api.ui.AIButton;
-
-public class MainActivity extends AppCompatActivity {
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         User currUser = new User();
-        ImageButton ibtnAsk = (ImageButton) findViewById(R.id.ibtnAsk);
+        ImageButton speakButton = (ImageButton) findViewById(R.id.ibtnAsk);
         final TextView tvResponse = (TextView) findViewById(R.id.tvResponse);
-        final InputAnalysis analyzer;
-        analyzer = new InputAnalysis(new InputAnalysis.ErrorHandler() {
-            @Override
-            public void onError(Throwable error) {
-                tvResponse.setText("Failed to configure response");
-            }
-        });
-
-        ibtnAsk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                analyzer.analyzeInput(new InputAnalysis.SuccessHandler() {
-                    @Override
-                    public void onComplete(String response) {
-                        tvResponse.setText(response);
-                    }
-                });
-            }
-        });
 
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 
 }
